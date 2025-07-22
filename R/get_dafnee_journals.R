f18ee990eee06def97352e7c23296db2ec881740
Nicolas Casajus <rdev.nc@gmail.com>
2025-07-22 09:34:49 +0200

    feat: get_dafnee_journals()

---
+++ R/get_dafnee_journals.R
@@ -0,0 +1,39 @@
+#' List DAFNEE journals
+#'
+#' @description
+#' The DAFNEE database (Database of Academia‑Friendly Journals in Ecology and
+#' Evolution, <https://dafnee.isem-evolution.fr/>) provides the business model
+#' and the academic friendly status of several journals in the field of Ecology
+#' and Evolution.
+#'
+#' The `faircitations` package provides a selection of 291 DAFNEE journals and
+#' this function returns information about these journals
+#'
+#' @return A `data.frame` three columns:
+#'   - `journal`, the name of the journal
+#'   - `business_model`, the business model of the journal (non-profit or
+#'     for-profit)
+#'   - `academic_friendly`, the academic friendly status of the journal (yes or
+#'     no)
+#'
+#' @export
+#'
+#' @examples
+#' # List DAFNEE journals in faircitations ----
+#' journals <- get_dafnee_journals()
+#'
+#' nrow(journals)
+#' head(journals)
+
+get_dafnee_journals <- function() {
+  data <- dafnee[, -1]
+  colnames(data)[1] <- "journal"
+
+  data$"business_model" <- gsub("^NP$", "Non-profit", data$"business_model")
+  data$"business_model" <- gsub("^FP$", "For-profit", data$"business_model")
+
+  data$"academic_friendly" <- gsub("^yes$", "Yes", data$"academic_friendly")
+  data$"academic_friendly" <- gsub("^no$", "No", data$"academic_friendly")
+
+  data
+}
