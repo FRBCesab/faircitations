17dab9b6f1e8e4e1e48bb7cd1157c8ee68ffd1b2
Nicolas Casajus <rdev.nc@gmail.com>
2025-07-23 09:56:05 +0200

    fix: improve doi cleaning

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,10 @@
 #' @noRd
 
 fp_clean_doi <- function(doi) {
-  doi <- gsub("https://doi.org/", "", doi)
-  doi <- gsub("doi:", "", doi)
   doi <- gsub("\\s", "", doi)
   doi <- tolower(doi)
+  doi <- gsub("http(s)?://(dx.)?doi.org/", "", doi)
+  doi <- gsub("doi:", "", doi)
 
   doi
 }
