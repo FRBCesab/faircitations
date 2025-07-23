795cad549248ea19d12e99b050a627f3ea6419a7
Nicolas Casajus <rdev.nc@gmail.com>
2025-07-23 10:12:22 +0200

    feat: implement fp_check_mailto()

--- R/utils.R
+++ R/utils.R
@@ -8,3 +8,16 @@ fp_clean_doi <- function(doi) {
 
   doi
 }
+
+#' @noRd
+
+fp_check_mailto <- function() {
+  if (is.null(options()$"openalexR.mailto")) {
+    stop(
+      "Be polite with OpenAlex API and run: ",
+      "`options(openalexR.mailto = 'your_email')`"
+    )
+  }
+
+  invisible(NULL)
+}
